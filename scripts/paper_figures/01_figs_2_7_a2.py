9c6410c48b2ccb08fa9240b46d93bb739b978792
Michele Coscia <michele.coscia@gmail.com>
2026-01-29 10:33:12 +0100

    Added HISCO support to 01_figs_2_7_a2.py

--- scripts/paper_figures/01_figs_2_7_a2.py
+++ scripts/paper_figures/01_figs_2_7_a2.py
@@ -1,10 +1,12 @@
-import rdata, warnings
+import rdata, warnings, sys
 import numpy as np
 import pandas as pd
 
 warnings.filterwarnings("ignore", category = UserWarning)
 
-dfs = rdata.read_rds("../../results/economic_complexity/province_all_bias_tbs.rds")
+hisco_tag = "_HISCO" if sys.argv[1] == "hisco" else ""
+
+dfs = rdata.read_rds("../../results/economic_complexity/province_all_bias_tbs{hisco_tag}.rds")
 df = pd.concat([dfs[x].to_pandas().unstack() for x in dfs if "without_neighbours" in x])
 df = df.reset_index()
 df.columns = ("occupation", "province", "value")
@@ -15,10 +17,10 @@ mcp = np.log(pd.pivot_table(data = mcp, index = "province", columns = "occupatio
 mcp = mcp.loc[mcp.sum(axis = 1).sort_values(ascending = False).index.values, mcp.sum().sort_values(ascending = False).index.values]
 row_order_all = mcp.index
 col_order_all = mcp.columns
-mcp.to_csv("fig_s1_bottom.csv", sep = "\t", index = False, header = False)
+mcp.to_csv("fig_a2_bottom{hisco_tag}.csv", sep = "\t", index = False, header = False)
 
 # Only Latin provinces, no language correction
-dfs = rdata.read_rds("../../results/economic_complexity/filtered_province_all_but_language_bias_tbs.rds")
+dfs = rdata.read_rds("../../results/economic_complexity/filtered_province_all_but_language_bias_tbs{hisco_tag}.rds")
 df = pd.concat([dfs[x].to_pandas().unstack() for x in dfs])
 df = df.reset_index()
 df.columns = ("occupation", "province", "value")
@@ -28,7 +30,7 @@ mcp = mcp.loc[mcp.sum(axis = 1).sort_values(ascending = False).index.values, mcp
 row_order = mcp.index
 col_order = mcp.columns
 max_value = mcp.max().max()
-mcp.to_csv("fig_1_b.csv", sep = "\t", index = False, header = False)
+mcp.to_csv("fig_2.csv", sep = "\t", index = False, header = False)
 
 provinces = set(mcp.index)
 
@@ -37,11 +39,11 @@ df = pd.read_csv("../../results/economic_complexity/province_table.csv", index_c
 # All provinces, no corrections
 mcp = np.log(df + 1)
 mcp = mcp.reindex(index = row_order_all, columns = col_order_all).fillna(0.0)
-mcp.to_csv("fig_s1_top.csv", sep = "\t", index = False, header = False)
+mcp.to_csv("fig_a2_top.csv", sep = "\t", index = False, header = False)
 
 # Only Latin provinces, no corrections
 df = df[df.index.isin(provinces)]
 mcp = np.log(df + 1)
 mcp = mcp.reindex(index = row_order, columns = col_order).fillna(0.0)
 min_value = (mcp[mcp > 0]).min().min()
-mcp.to_csv("fig_1_a.csv", sep = "\t", index = False, header = False)
+mcp.to_csv("fig_7.csv", sep = "\t", index = False, header = False)
