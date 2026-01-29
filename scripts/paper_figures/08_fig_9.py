70755952f218989ea03790aed446bc46101c422d
Michele Coscia <michele.coscia@gmail.com>
2026-01-29 11:18:11 +0100

    Rename 08_fig_8.py to 08_fig_9.py for new figure order

--- scripts/paper_figures/08_fig_9.py
+++ scripts/paper_figures/08_fig_9.py
@@ -5,4 +5,4 @@ ecis = pd.read_csv("../../results/economic_complexity/filtered_province_all_but_
 ecis.index = ecis.index.map(lambda x: x if len(x) <= 16 else f"{x[:13]}...")
 
 ecis = ecis.groupby(by = "province").quantile(q = [0.1, 0.25, 0.5, 0.75, 0.9]).reset_index()
-pd.pivot_table(data = ecis, index = "province", columns = "level_1", values = "eci").reset_index().sort_values(by = 0.50, ascending = False).to_csv("fig_8.csv", sep = "\t", index = False, header = False)
+pd.pivot_table(data = ecis, index = "province", columns = "level_1", values = "eci").reset_index().sort_values(by = 0.50, ascending = False).to_csv("fig_9.csv", sep = "\t", index = False, header = False)
